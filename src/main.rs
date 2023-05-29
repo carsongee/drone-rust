4701bb11b674c66cfd779227ca96dbaef0ea763d
Carson Gee <x@carsongee.com>
2023-05-29 12:07:04 -0600

    Adding command running

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use clap::Parser;
 
+
 #[derive(Parser, Debug)]
 #[command(version, about = "Drone plugin for Rust", long_about = None)]
 struct Args {
     /// Pattern to look for
-    #[arg(short, long, env = "PLUGIN_PATTERN")]
-    pattern: String,
+    #[arg(short, long, env = "PLUGIN_COMMANDS", value_delimiter = ',')]
+    commands: Vec<String>,
+}
+
+fn run_command(command: &String, dry_run: bool) {
+    println!("{}", format!("> {command}"));
+    if dry_run {
+        println!("Dry run");
+    }
 }
 
 fn main() {
     let args = Args::parse();
-    println!("Hello {}!", args.pattern);
+    for command in &args.commands {
+        run_command(command, true);
+    }
     println!("Answer {}", drone_rust::answer());
 }
